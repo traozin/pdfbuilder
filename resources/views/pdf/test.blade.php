52859c14e0f95c2bd79bca422e46ea473917e00d
Antônio Neto <acm.neto1999@gmail.com>
2025-07-09 22:46:37 -0300

    :sparkles: feat: adicionar rota de teste

---
+++ resources/views/pdf/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PDF de Teste</title>
+</head>
+<body>
+    <h1>Funcionou!</h1>
+    <p>PDF gerado com Snappy</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Barryvdh\Snappy\Facades\SnappyPdf;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-pdf', function () {
+    return SnappyPdf::loadView('pdf.test')->download('teste.pdf');
 });
