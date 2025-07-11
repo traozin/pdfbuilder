b4fd5449b03617e1c11c883f63bc1ef70fe94625
Antônio Neto <acm.neto1999@gmail.com>
2025-07-10 23:33:27 -0300

    :recycle: refac: remover rota de teste e redirecionar para geração de PDF

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
-use Barryvdh\Snappy\Facades\SnappyPdf;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test-pdf', function () {
-    return SnappyPdf::loadView('pdf.test')->stream('teste.pdf');
+    return redirect()->route('pdf.gerar');
 });
 
 Route::get('/gerar-pdf', [PdfController::class, 'gerarPdf'])->name('pdf.gerar');
